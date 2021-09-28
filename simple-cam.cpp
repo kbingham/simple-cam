kbingham/simple-cam

commit 14529b6d1c4a6d405b23157eca8a4b0bec632e00

simple-cam: Early return if no cameras are found on the system
Failing to do so, the codepath will segfault while trying to acquire
a non-existent camera.

Signed-off-by: Umang Jain <email@uajain.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

simple-cam.cpp
@@ -307,6 +307,13 @@ int main()
 	for (std::unique_ptr<Request> &request : requests)
 		camera->queueRequest(request.get());
 
+	if (!cm->cameras().size()) {
+		std::cout << "No cameras were identified on the system."
+			  << std::endl;
+		cm->stop();
+		return EXIT_FAILURE;
+	}
+
 	/*
 	 * --------------------------------------------------------------------
 	 * Run an EventLoop

commit 6cb63c923c49a8763ec99eb7a9285ef79558b8f2

simple-cam: Move camera detection check to where it belongs
Commit 14529b6d1c4a ("simple-cam: Early return if no cameras are found
on the system") added a check to ensure that at least one camera is
present in the system to avoid a crash otherwise. The check was however
placed towards the end of the main() function, way after the camera is
accessed, possibly due to a rebase conflict. Move it before accessing
the first camera.

While at it, replace usage of std::vector::size() with
std::vector::empty() which expresses the intent better.

Fixes: 14529b6d1c4a ("simple-cam: Early return if no cameras are found on the system")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Paul Elder <paul.elder@ideasonboard.com>
Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

simple-cam.cpp
@@ -163,13 +163,21 @@ int main()
 	 * Application lock usage of Camera by 'acquiring' them.
 	 * Once done with it, application shall similarly 'release' the Camera.
 	 *
-	 * As an example, use the first available camera in the system.
+	 * As an example, use the first available camera in the system after
+	 * making sure that at least one camera is available.
 	 *
 	 * Cameras can be obtained by their ID or their index, to demonstrate
 	 * this, the following code gets the ID of the first camera; then gets
 	 * the camera associated with that ID (which is of course the same as
 	 * cm->cameras()[0]).
 	 */
+	if (cm->cameras().empty()) {
+		std::cout << "No cameras were identified on the system."
+			  << std::endl;
+		cm->stop();
+		return EXIT_FAILURE;
+	}
+
 	std::string cameraId = cm->cameras()[0]->id();
 	camera = cm->get(cameraId);
 	camera->acquire();
@@ -385,13 +393,6 @@ int main()
 	camera->start();
 	for (std::unique_ptr<Request> &request : requests)
 		camera->queueRequest(request.get());
-
-	if (!cm->cameras().size()) {
-		std::cout << "No cameras were identified on the system."
-			  << std::endl;
-		cm->stop();
-		return EXIT_FAILURE;
-	}
 
 	/*
 	 * --------------------------------------------------------------------